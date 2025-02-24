d3cb91532383235d992fc40c40152b83a7d3afb7
Oleksandr Melenchuk <forandback91@gmail.com>
2025-02-24 15:27:37 +0200

    ready_to_go

--- Home_work_correct_phone_number.py
+++ Home_work_correct_phone_number.py
@@ -1,24 +1,45 @@
 import re
-
-
 """Функція для нормалізації телефоних номерів
      до стандартного формату """
 def normalize_phone(phone_number):
-    # Регулярний вираз для сортування номера
-    pattern = r'\+*\d+'
+     
 
-    # Відокремлення зайвих символів
-    check = re.findall(pattern, phone_number)
+     try:
+          # Перевірка на відповідність типу
+          if not isinstance(phone_number, str):
+               raise TypeError('Введено невірний тип даних')
+          
+          
+          # Регулярний вираз для сортування номера
+          pattern = r'\+?\d+'
 
-    # Поєднання в одну строку
-    result = ''.join(check)
+          # Відокремлення зайвих символів
+          check = re.findall(pattern, phone_number)
 
-    # Перевірка на наявність регіонального коду
-    if result.find('38') == -1:
-            result = '38' + result
+          # Поєднання в одну строку
+          result = ''.join(check)
+          
+          # Перевірка номера на мінімальну та максимальну кількість символів
+          if len(result) < 9 or len(result) > 13:
+               raise ValueError("Невірна довжина номера")
 
-    # Перевірка на наявність префіксу
-    if result.find('+') == -1:
-            result = '+' + result
-        
-    return result
+          # Перевірка на наявність регіонального коду та префіксу '+'
+          if result.startswith('380'):
+               result = '+' + result
+
+          elif result.startswith('0'):
+               result = '+38' + result
+          
+          elif result.startswith('80'):
+               result = '+3' + result
+
+     #Обробка винятків таких як невірний тип, значення
+     except TypeError:
+          print('Введено невірний тип даних')
+          return None
+     
+     except ValueError:
+          print(f"{result} Некоректний номер телефону")
+          return None
+     
+     return result
--- Home_work_lucky_number.py
+++ Home_work_lucky_number.py
@@ -9,11 +9,18 @@ def get_numbers_ticket(min=1, max=1000 , quantity=10):
     #Множина для ініціалізації циклу
     lucky_list = set()
 
+    #Список для перевірки вхідних даних
+    variables = [min, max, quantity]
+
     try:
-        #Перевірка чи значення в заданому діапазоні.Якщо ні повертає пустий список
-        if min < 1 or max > 1000 or quantity > max + min:
-            print("Невірно заданий діапазон чисел")
-            return list(lucky_list)
+        # Перевірка чи вказаний тип відповідає int
+        for x in variables:
+                if not isinstance(x, int):
+                    raise TypeError
+                
+        #Перевірка чи значення в заданому діапазоні
+        if min < 1 or max > 1000 or quantity > (max - min + 1) or min > max:
+            raise ValueError
 
         '''Цикл для заповнення унікальними випадковими числами 
             до поки кількість чисел не буде = quantity '''
@@ -22,22 +29,15 @@ def get_numbers_ticket(min=1, max=1000 , quantity=10):
             #Якщо число унікальне додає до множини
             lucky_list.add(random.randint(min, max))
 
+    #Обробка винятків таких як невірний тип, значення
+    except ValueError:
+         print("Невірно вказане значення")
+         return []
     except TypeError:
-        print("Невірний тип даних")
+        print("Невірно вказаний тип")
+        return []
 
     # Повертає відстортований список
     return sorted(lucky_list)
 
-
-
-
-
-print(get_numbers_ticket([], '100', 10))
-
-
-
-
-
-# raise ValueError("Введіть коректні значення для Min не меньше 1,\n" 
-#        "для Max не більше 1000. Кількість номерів не може перевищувати\n"
-#        "загальної кількості чисел")
+print(get_numbers_ticket(150,100,10))
